b59487ba4a3948c1db73e31a95d32b991f4b1b50
tvntvn <taneli.makihannu@gmail.com>
2023-02-15 17:40:07 +0200

    changed enemycount

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ import { Enemy, Player, allEnemies } from "./classes.js";
 
 export let pause = true;
 startScreen();
-const ENEMY_NUM = 1;
+const ENEMY_NUM = 4;
 let startTime;
 let startTime2;
 let startTime3;
