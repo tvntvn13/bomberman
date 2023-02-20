90172c90c1943a2eeeafb4aea9662536a079e584
Peter Vekony <peter.vekony@protonmail.com>
2023-02-20 14:43:20 +0200

    comment out unnecessary enemy movement code

--- js/classes.js
+++ js/classes.js
@@ -267,7 +267,7 @@ export class Enemy extends MovingElement {
   }
   moveUp() {
     if (super.moveUp()) {
-      delete allEnemies[`${this.x}:${this.y}`];
+    //   delete allEnemies[`${this.x}:${this.y}`];
       // allEnemies[`${this.x}:${this.y-1}`] = this;
       return true;
     } else {
@@ -276,7 +276,7 @@ export class Enemy extends MovingElement {
   }
   moveDown() {
     if (super.moveDown()) {
-      delete allEnemies[`${this.x}:${this.y}`];
+    //   delete allEnemies[`${this.x}:${this.y}`];
       // allEnemies[`${this.x}:${this.y+1}`] = this;
       return true;
     } else {
@@ -285,7 +285,7 @@ export class Enemy extends MovingElement {
   }
   moveLeft() {
     if (super.moveLeft()) {
-      delete allEnemies[`${this.x}:${this.y}`];
+    //   delete allEnemies[`${this.x}:${this.y}`];
       // allEnemies[`${this.x-1}:${this.y}`] = this;
       return true;
     } else {
@@ -294,7 +294,7 @@ export class Enemy extends MovingElement {
   }
   moveRight() {
     if (super.moveRight()) {
-      delete allEnemies[`${this.x}:${this.y}`];
+    //   delete allEnemies[`${this.x}:${this.y}`];
       // allEnemies[`${this.x+1}:${this.y}`] = this;
       return true;
     } else {
